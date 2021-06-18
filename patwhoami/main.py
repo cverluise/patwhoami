28befc79d1af7ac2efdc5d22bb1506dfb462e39d
cverluise <cyril.verluise@gmail.com>
2021-06-18 23:57:39 +0200

    :tada: add pred sub module (CLI)

--- patwhoami/main.py
+++ patwhoami/main.py
@@ -1,9 +1,10 @@
 import typer
-from patwhoami import prep
+from patwhoami import prep, pred
 
 app = typer.Typer()
 
 app.add_typer(prep.app, name="prep")
+app.add_typer(pred.app, name="pred")
 
 
 if __name__ == "__main__":
---
+++ patwhoami/pred.py
@@ -0,0 +1,32 @@
+import json
+import typer
+import spacy
+from pathlib import Path
+
+app = typer.Typer()
+
+
+@app.command()
+def cats(file: Path, model: Path):
+    """
+
+    Arguments:
+        file:
+        model:
+
+    **Usage:**
+        ```shell
+        ```
+
+    """
+    nlp = spacy.load(model)
+    with open(file, "r") as lines:
+        for line in lines:
+            line = json.loads(line)
+            doc = nlp(line["text"])
+            line.update(doc.cats)
+            typer.echo(json.dumps(line))
+
+
+if __name__ == "__main__":
+    app()
